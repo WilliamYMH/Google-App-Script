8807ad4a7bf00b84e103e888359e6d1d2d899b87
William <williamyesid.10@gmail.com>
2020-09-23 09:48:40 -0500

    added generar documento, generar pdf

--- script.js
+++ script.js
@@ -1,45 +1,60 @@
 function onOpen(e){
     SpreadsheetApp.getUi().createMenu('Script-WYMH')
     .addItem('Combinar correspondencia', 'combinarCorrespondencia')
+   .addItem('Generar document', 'generarArchivo')
+  .addItem('Generar PDF', 'generarPdf')
     .addToUi();
   }
-  
-  function combinarCorrespondencia() {
-    
-    let tabla = getTabla();
-    //Logger.log(tabla);
-    if(tabla){
-      for(let i=2; i<tabla.length; i++) {
-        //Logger.log(tabla[i][7]);
-        GmailApp.sendEmail((tabla[i][7]), 
-                           "Combinacion de correspondecia",
-                           "Estimado (a) estudiante: "+tabla[i][3]+" "+tabla[i][4]+" \nNos permite informarle su reporte en el curso Computacion en la nube."+
-                           "\nCalificacion: "+tabla[i][5]+
-                           ".\nObservaciones: "+tabla[i][6]+
-                           ".\nPuesto en el grupo: "+tabla[i][1]+
-                           ".\nFelicitaciones y exitos en su vida academica y profesional.");
-      }
-      return;
-    }
-  }
 
- function getTabla() {
+function combinarCorrespondencia(){
+let sheet = SpreadsheetApp.getActiveSheet();
+let startRow = 2;
+let rows = sheet.getLastRow();
+let dataRange = sheet.getRange(startRow, 1, rows, rows);
+let data = dataRange.getValues();
+  
+for (let i in data) {
+  let row = data[i];
+  let emailAddress = row[2]; 
+  let subject = 'Combinacion Correspondencia';
+  let message = "Estimado (a) Estudiante "+row[0]+" "+row[1]+" "+"Código " +row[3]+"\n"+
+    "Nos permitimos informarle su calificación definitiva en el curso Computacion en la nube:"+"\n"+
+      "Calificación: "+row[4]+"\n"+
+        "Observaciones: " +row[5]+"\n"+
+          "Felicitaciones y éxitos en su vida académica y profesional."
+        
+  GmailApp.sendEmail(emailAddress, subject, message);
+}
+}
+
+
+function generaArchivo(){
+  
+ let sheet = SpreadsheetApp.getActiveSheet();
+ let data = sheet.getDataRange().getValues();
+ const maxRow = hojas[0].getLastRow()-1;
+ let document = DocumentApp.openById('1L6Ds1zXK0JupeA8HpT6PNUv31ZaEbrCuCc8r3MTUeao');
+ let columns = [
+     ['Nombre','Apellido','Correo','Código','calificación','Observaciones']
+   ];
+  for(let i = 1; i<maxRow; i++){
+     let fila = data[i];
     
-    var sheet = SpreadsheetApp.getActiveSheet();
-    if(sheet){
-      Logger.log('sheeet');
-      let colum = 8;
-      let rows = sheet.getLastRow();
-      let table = [];
-      //Logger.log(rows);
-      for(let i=1; i<rows; i++) {
-        table[i] = new Array(colum);
-        for(let j=1; j<colum; j++) {
-          table[i][j] = sheet.getRange(i, j).getValue();
-        }
-      }
-      return table;
-    }
-    return null;
+     let nombre = fila[0];
+     let apellido = fila[1];
+     let correo = fila[2];
+     let codigo = fila[3];
+     let nota = fila[4];
+     let observaciones = fila[5];
+    
+    columns.push([nombre,apellido,correo,codigo,nota,observaciones]);
+    
   }
+  document.getBody().appendTable(columns);
+}
+
+function generarPdf(){
+  let document = DocumentApp.openById('1L6Ds1zXK0JupeA8HpT6PNUv31ZaEbrCuCc8r3MTUeao');
+  DriveApp.createFile(document.getAs('application/pdf'));
+}
  
